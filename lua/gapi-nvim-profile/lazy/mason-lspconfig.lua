1ad0f2ab74206f40c71f13be5a36bd5d86296b7d
Gasper Sodja <sodjagas@gmail.com>
2025-05-12 14:47:01 +0200

    upgrade

--- lua/gapi-nvim-profile/lazy/mason-lspconfig.lua
+++ lua/gapi-nvim-profile/lazy/mason-lspconfig.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { 'williamboman/mason.nvim' },
   config = function()
     require('mason-lspconfig').setup({
-        ensure_installed = {'lua_ls', 'rust_analyzer', 'pyright', 'wgsl_analyzer', 'zls', 'cssls', 'jsonls'}-- Add your desired servers
+        ensure_installed = {'lua_ls', 'rust_analyzer', 'pyright', 'wgsl_analyzer', 'zls', 'cssls', 'jsonls', 'html', 'tailwindcss', }-- Add your desired servers
     })
     local lspconfig = require('lspconfig')
 
@@ -28,5 +28,23 @@ return {
     lspconfig.zls.setup({ capabilities = capabilities })
     lspconfig.pyright.setup({ capabilities = capabilities })
     lspconfig.cssls.setup({ capabilities = capabilities })
+    lspconfig.html.setup({
+      capabilities = capabilities,
+      init_options = {
+        configurationSection = { "html", "css", "javascript" },
+        embeddedLanguages = { css = true, javascript = true },
+        provideFormatter = true,
+      },
+    })
+    lspconfig.tailwindcss.setup({
+      capabilities = capabilities,
+      filetypes = { "html", "css", "scss", "javascript", "typescript", "rust" },
+      init_options = {
+        userLanguages = {
+          rust = "html",
+          htmx = "html",
+        },
+      },
+    })
   end
 }
